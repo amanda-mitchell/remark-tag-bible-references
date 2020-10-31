a94506d23e2d02ff97883ee02f066eaa4d09d463
Amanda Mitchell <she@mandymitchell.me>
2020-10-31 12:34:53 -0700

    chore: Fix tests.

--- __tests__/predicates.js
+++ __tests__/predicates.js
@@ -3,24 +3,24 @@ const { isText, isLink } = require('../predicates');
 
 describe('isText', () => {
   test('it returns true for a text node', () => {
-    expect(isText(build('text', 'hi'))).toEqual(true);
+    expect(isText(build('text', 'hi'))).toBeTruthy();
   });
 
   test('it returns false for something else', () => {
-    expect(isText(build('somethingElse', 'hi'))).toEqual(false);
+    expect(isText(build('somethingElse', 'hi'))).toBeFalsy();
   });
 });
 
 describe('isLink', () => {
   test('it returns true for a link node', () => {
-    expect(isLink(build('link', [build('text', 'hi')]))).toEqual(true);
+    expect(isLink(build('link', [build('text', 'hi')]))).toBeTruthy();
   });
 
   test('it returns true for a linkReference node', () => {
-    expect(isLink(build('linkReference', [build('text', 'hi')]))).toEqual(true);
+    expect(isLink(build('linkReference', [build('text', 'hi')]))).toBeTruthy();
   });
 
   test('it returns false for something else', () => {
-    expect(isLink(build('somethingElse', 'hi'))).toEqual(false);
+    expect(isLink(build('somethingElse', 'hi'))).toBeFalsy();
   });
 });
